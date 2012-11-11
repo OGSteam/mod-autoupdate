5a699b8f083075ec7510e1065be63243b0834823
darknoon29 <darknoon29@localhost>
2012-11-11 21:48:57 +0100

    R?cup?ration des mods sur le d?p?t Bitbucket de l'ogsteam.

--- mod_list.php
+++ mod_list.php
@@ -7,85 +7,116 @@
 * created	: 16/01/2012
 * modified	: 16/01/2012
 */
-$ban_mod = array(
-'sql',//Réservé au developpeurs
-'mplogger',//Obsolète
-'naqogsplugin',//Obsolète
-'ogsfox',//Obsolète
-'quimobserve',//Obsolète
-'packmod',//Réservé au developpeurs
-'modupdate',//Obsolète
-'market',//Obsolète
-'myspeach', //Obsolète
-'analyseI', // Problème Installation
-'arcade', // Proche du Tag
-'autonomie', // Bug en cours
-'copylocal',
-'calculressources',
-'communication',
-'convertisseur',
-'decolonisation',
-'defence',
-'empire',
-'energie',
-'federationcommerciale',
-'gameogame',
-'guerres',
-'hof', //obsolète
-'lesmip',
-'litesearch',
-'maj',
-'missiles',
-'news',
-'ocartomips',
-'ogsCalc',
-'ogsSign',
-'rcConv',
-'rcSave',
-'rechercheAlliance',
-'rechercheAlly',
-'ressources',
-'restyler',
-'sogsrov',
-'update',
-'refinder'
-);
 
-$mod_list = array(
-'ogspy', //outils
-'advspy', //Mods
-'allyranking',
-'attaques',
-'autoupdate',
-'bthof',
-'cdr',
-'densite',
-'exchange',
-'expedition',
-'flottes',
-'frame',
-'gestionmod',
-'graviton',
-'hofrc',
-'leslunes',
-'majvisu',
-'pandore',
-'presentationalliance',
-'production',
-'quimsonde',
-'rechercheplus',
-'recycleurs',
-'reinette',
-'tempsvol',
-'usines',
-'vaisseaux',
-'varally',
-'xtense',
-'zonecheck'
-);
-/*
-$mod_list = array(
-'autoupdate',
-'xtense'
-);*/
+
+function getRepositorylist(){
+    
+    $mod_list = array();
+    
+    $repo_link ='https://api.bitbucket.org/1.0/users/ogsteam';
+    if(time() > (mod_get_option('LAST_REPO_LIST') + mod_get_option('CYCLEMAJ') * 3600)){
+        @copy($repo_link, './mod/autoupdate/tmp/repo_list.json');
+        mod_set_option('LAST_REPO_LIST', time());
+    }
+    $mod_file = file_get_contents('./mod/autoupdate/tmp/repo_list.json');	
+
+    $result = utf8_encode($mod_file);
+    $data = json_decode($result, true);
+    //On récupère ce que l'on a besoin dans la structure JSON
+    foreach($data['repositories'] as $id)
+    {
+        $mods_tmp[] = array('nom' => $id["slug"],
+        'description' => $id["description"],
+        'resource_uri' => $id["resource_uri"],
+        'owner' =>$id["owner"],
+        'is_fork' =>$id["is_fork"],
+        'fork_resource_uri' => $id["fork_of"]["resource_uri"],
+        'fork_owner' =>$id["fork_of"]["owner"]);
+    }
+    //Mise en Forme pour le mod (Fait en 2 partie pour plus de lisibilité)
+    foreach($mods_tmp as $mod)
+    {
+        if(preg_match("/-/",$mod["nom"])){
+            $mod_name = explode('-',$mod["nom"]);
+            $mod["nom"] = $mod_name[1];
+        }
+                    
+        if($mod["is_fork"] == false){            
+            $mod_list[] = array('nom' => $mod["nom"],
+            'description' => $mod["description"],
+            'resource_uri' => $mod["resource_uri"],
+            'owner' => $mod["owner"]);
+        }else{
+            $mod_list[] = array('nom' =>$mod["nom"],
+            'description' => $mod["description"],
+            'resource_uri' => $mod["fork_resource_uri"],
+            'owner' => $mod["fork_owner"]);            
+        }
+        
+     }
+    //print_r($mod_list);
+    return ($mod_list);
+     
+}
+
+function getRepositoryDetails($repoName){
+    
+    $liste_mods = getRepositorylist();
+    //print_r($liste_mods);
+    foreach($liste_mods as $mod){
+
+        if($mod['nom'] == $repoName){
+            
+            return array('nom' =>$mod["nom"],
+                'description' => $mod["description"],
+                'resource_uri' => $mod["resource_uri"],
+                'owner' => $mod["owner"]);  
+        }
+    }
+return false;
+}
+
+
+function getRepositoryVersion($Reponame, $isMod = true ){
+
+    $repo_details = getRepositoryDetails($Reponame);
+    if($repo_details == false) return "-1";
+    
+    $repo_link = 'https://api.bitbucket.org'.$repo_details['resource_uri'].'/tags';
+   
+    if( !ini_get('safe_mode') ) set_time_limit(30);
+    
+    if(time() > (mod_get_option('LAST_MOD_UPDATE-'.$Reponame) + mod_get_option('CYCLEMAJ') * 3600)){
+        @copy($repo_link, './mod/autoupdate/tmp/'.$Reponame.'.json');
+        mod_set_option('LAST_MOD_UPDATE-'.$Reponame, time());
+    }
+     if(file_exists('./mod/autoupdate/tmp/'.$Reponame.'.json')){
+        $api_list = file_get_contents('./mod/autoupdate/tmp/'.$Reponame.'.json');	
+
+        $result = utf8_encode($api_list);
+        $data = json_decode($result, true);
+        $version_list = array_keys($data);
+        // Supression de l'étiquette tip
+        $tip_id = array_search('tip', $version_list);
+        unset($version_list[$tip_id]);
+
+        //tri de la liste de versions pour obtenir la dernière :
+        rsort($version_list);
+
+        if(count($version_list) > 0)
+        {
+            return $version_list[0];
+        }else
+        {
+            log_('mod', 'Pas de version de Production pour le mod '.$Reponame);
+            return "-1";
+        }
+     }else{
+         log_('mod', 'Impossible de récupérer le fichier de version du mod '.$Reponame);
+         mod_del_option('LAST_MOD_UPDATE-'.$Reponame);
+         return "-1";
+         
+     }
+}
+
 ?>
