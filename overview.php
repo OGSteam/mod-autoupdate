8b8979aefa09cc3e4398a005e923bb7ef2706aeb
Anthony <darknoon29@users.noreply.github.com>
2015-05-13 17:21:45 +0200

    Added tag 2.1.2 for changeset 13490b6fc426

---
+++ overview.php
@@ -0,0 +1,163 @@
+<?php
+/** $Id: tableau.php 7672 2012-08-05 21:33:46Z darknoon $ **/
+/**
+* autoupdate.php Met à jour les mods depuis le serveur
+* @package [MOD] AutoUpdate
+* @author Bartheleway <contactbarthe@g.q-le-site.webou.net>
+* @version 1.0
+* created	: 27/10/2006
+* modified	: 18/01/2007
+*/
+
+if (!defined('IN_SPYOGAME')) die("Hacking attempt");
+
+require_once("mod/autoupdate/mod_list.php");
+require_once("views/page_header.php");
+
+$query = "SELECT `active` FROM `".TABLE_MOD."` WHERE `action`='autoupdate' AND `active`='1' LIMIT 1";
+if (!$db->sql_numrows($db->sql_query($query))) die("Hacking attempt");
+
+/*On récupère la liste des mods installés*/
+
+$sql = "SELECT title,root,version from ".TABLE_MOD;
+$res = $db->sql_query($sql,false,true);
+
+$i=0;
+while (list($modname,$modroot,$modversion) = $db->sql_fetch_row($res))
+{
+    $installed_mods[$i]['name'] 		= $modname;	
+    $installed_mods[$i]['root']		= $modroot;
+    $installed_mods[$i++]['version'] 	= $modversion;
+}
+
+// Recupération des Mods disponible sur l'ogsteam
+//$data = getmodlist();
+//$mod_names = array_keys($data); // Récupération des clés
+
+
+?>
+<div align="center"><?php echo $lang['autoupdate_tableau_info']; ?></div>
+<br />
+<table width='700'>
+	<tr>
+		<td class='c' colspan='100'><?php echo $lang['autoupdate_tableau_toolinstall']; ?></td>
+	</tr>
+    <tr>
+		<td class='c'><?php echo $lang['autoupdate_tableau_nametool']; ?></td>
+		<td class='c' width = "50"><?php echo $lang['autoupdate_tableau_version']; ?></td>
+		<td class='c' width = "50"><?php echo $lang['autoupdate_tableau_versionSVN']; ?></td>
+        <?php if($user_data['user_admin'] == 1 || $user_data['user_coadmin'] == 1) echo '<td class=\'c\' width = "100">'.$lang['autoupdate_tableau_action'].'</td>'; ?>
+		<?php if(mod_get_option("MAJ_TRUNK") == 1){ echo "<td class='c' width = '50'>"; echo $lang['autoupdate_tableau_versionTrunk']."</td>"; }?>
+	</tr>
+    <tr>
+		<th>OGSpy</th><th>
+<?php 
+echo $server_config["version"]."</th>";
+$cur_version = getRepositoryVersion('ogspy', false);
+ if($cur_version == '-1'){
+    echo "\t\t<th>".$lang['autoupdate_tableau_norefered']."</th>\n";
+    $cur_version = 0;
+ }
+ else{
+    echo "\t\t<th>".$cur_version."</th>\n";
+ }
+echo "<th>";
+if (version_compare($cur_version,$server_config["version"],">"))
+{
+    $ziplink = "<a href='index.php?action=autoupdate&sub=tool_upgrade&tool=ogspy&tag=".$cur_version."'>".$lang['autoupdate_tableau_uptodate']."</a>";
+    echo "<font color='lime'>".$ziplink."</font>";
+} else {
+    echo "Aucune";
+}
+echo "</th>";
+if(mod_get_option("MAJ_TRUNK") == 1){
+	echo "<th>";
+	$ziplink = "<a href='index.php?action=autoupdate&sub=tool_upgrade&tool=ogspy&tag=trunk'>Télécharger</a>";
+	echo "<font color='lime'>".$ziplink."</font>";
+	echo "</th>";
+}
+?>
+	</tr>
+    <tr>
+		<td class='c' colspan='100'></td>
+	</tr>
+	<tr>
+		<td class='c' colspan='100'><?php echo $lang['autoupdate_tableau_modinstall']; ?></td>
+	</tr>
+	<tr>
+		<td class='c'><?php echo $lang['autoupdate_tableau_namemod']; ?></td>
+		<td class='c' width = "50"><?php echo $lang['autoupdate_tableau_version']; ?></td>
+		<td class='c' width = "50"><?php echo $lang['autoupdate_tableau_versionSVN']; ?></td>
+        <?php if($user_data['user_admin'] == 1 || $user_data['user_coadmin'] == 1) echo '<td class=\'c\' width = "100">'.$lang['autoupdate_tableau_action'].'</td>'; ?>
+		<?php if(mod_get_option("MAJ_TRUNK") == 1){ echo "<td class='c' width = '50'>"; echo $lang['autoupdate_tableau_versionTrunk']."</td>"; }?>
+	</tr>
+<?php	
+	
+	// 
+for ($i=0 ; $i<count($installed_mods) ; $i++) {
+    if (substr($installed_mods[$i]['name'], 0, 5) != "Group") {
+        echo "\t<tr>\n";
+        echo "\t\t<th>".$installed_mods[$i]['name']."</th>\n";
+        echo "\t\t<th>".$installed_mods[$i]['version']."</th>\n"; 
+
+        $cur_modroot = $installed_mods[$i]['root'];
+        $cur_version =getRepositoryVersion($cur_modroot);
+
+        if($cur_version == '-1'){
+           echo "\t\t<th>".$lang['autoupdate_tableau_norefered']."</th>\n";
+           $cur_version = 0;
+        }
+        else{
+           echo "\t\t<th>".$cur_version."</th>\n";
+        }
+         
+        if($user_data['user_admin'] == 1 || $user_data['user_coadmin'] == 1) {
+            echo "\t\t<th>";
+            if (!is_writable("./mod/".$installed_mods[$i]['root']."/")) echo "<a title='Pas de droit en écriture sur:./mod/".$installed_mods[$i]['root']."'><font color=red>(RO)</font></a>";
+            else {
+                if (version_compare($cur_version, $installed_mods[$i]['version'],">"))
+                {
+                    $ziplink = "<a href='index.php?action=autoupdate&sub=mod_upgrade&mod=".$cur_modroot."&tag=".$cur_version."'>".$lang['autoupdate_tableau_uptodate']."</a>";
+                    echo "<font color='lime'>".$ziplink."</font>";
+                } else {
+                    echo "Aucune";
+                }
+            }
+            echo "</th>\n";
+            if(mod_get_option("MAJ_TRUNK") == 1){
+                echo "\t\t<th>";
+                $ziplink = "<a href='index.php?action=autoupdate&sub=mod_upgrade&mod=".$cur_modroot."&tag=trunk'>Télécharger</a>";
+                echo "<font color='lime'>".$ziplink."</font>";
+                echo "</th>\n";
+            }
+        }
+
+    }
+    echo "\t</tr>\n";
+}
+
+
+ 	if ($user_data["user_admin"] == 1 || $user_data['user_coadmin'] == 1) {
+		// Proposer le lien vers le panneau d'administration des modules
+		
+		?>
+	<tr>
+		<td class="c" colspan="100"><?php echo $lang['autoupdate_tableau_link']; ?></td>
+	</tr>
+	<tr>
+		<th colspan="100"><a href="index.php?action=administration&subaction=mod"><?php echo $lang['autoupdate_tableau_pageadmin']; ?></a></th>
+	</tr>
+	<tr>
+		<th colspan="100"><a href="http://www.ogsteam.fr">OGSteam.fr</a></th>
+	</tr>
+		<?php
+	}
+	?>
+</table>
+<?php
+echo '<br />'."\n";
+echo 'AutoUpdate '.$lang['autoupdate_version'].' '.versionmod();
+echo '<br />'."\n";
+echo $lang['autoupdate_createdby'].' Jibus '.$lang['autoupdate_and'].' Bartheleway.</div>';
+require_once("views/page_tail.php");
+?>
---
+++ update.php
@@ -0,0 +1,22 @@
+<?php
+/** $Id: update.php 7672 2012-08-05 21:33:46Z darknoon $ **/
+/**
+* update.php Met à jour le mod
+* @package [MOD] AutoUpdate
+* @author Bartheleway <contactbarthe@g.q-le-site.webou.net>
+* @version 1.0a
+* created	: 27/10/2006
+* modified	: 19/01/2007
+*/
+if (!defined('IN_SPYOGAME')) {
+    die("Hacking attempt");
+}
+	
+$mod_folder= "autoupdate";
+$mod_name = "autoupdate";
+
+update_mod($mod_folder,$mod_name);
+mod_set_option ( "CYCLEMAJ", "24");
+mod_set_option ( "MAJ_TRUNK", "0");
+
+?>
