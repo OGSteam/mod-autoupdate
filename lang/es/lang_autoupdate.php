a96bd693432cded55324b3ade8cfd8d6e0ef758c
Romain Malochet <roms0406@gmx.fr>
2016-08-31 20:57:42 +0200

    Ajout langues anglaise (en), br?silienne (pt_BR) et espagnol partiel (es)

---
+++ lang/es/lang_autoupdate.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Autoupdate Language File
+ * @package [Mod] Autoupdate
+ * @subpackage i18n
+ * @author Bartheleway <contactbarthe@g.q-le-site.webou.net>
+ * @copyright Copyright &copy; 2016, http://ogsteam.fr/
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version 2.1.9
+ */
+
+$lang['autoupdate_autoupdate_table'] = "actualización de tabla";
+$lang['autoupdate_autoupdate_admin'] = "Administración";
+$lang['autoupdate_autoupdate_down'] = "descarga mods";
+$lang['autoupdate_tableau_info'] = "Cuando se actualizan los mods, los archivos de la postal sustituyan a los viejos.";
+$lang['autoupdate_tableau_error3'] = "Autoupdate atención no se permite escribir en la carpeta mod y no se puede instalar nada";
+$lang['autoupdate_tableau_error4'] = "No hay versión de producción disponible para este mod";
+$lang['autoupdate_tableau_namemod'] = "Nombre del módulo";
+$lang['autoupdate_tableau_authormod'] = "módulo de autor";
+$lang['autoupdate_tableau_descmod'] = "Descripcion";
+$lang['autoupdate_tableau_nametool'] = "Nombre del instrumento";
+$lang['autoupdate_tableau_authtool'] = "Auteur de l'outil";
+$lang['autoupdate_tableau_modinstall'] = "Modules installés - Recherche des mises à jour sur OGSteam.fr";
+$lang['autoupdate_tableau_toolinstall'] = "Outils installés - Recherche des mises à jour sur OGSteam.fr";
+$lang['autoupdate_tableau_modnoinstall'] = "Modules non installés - Recherche des mods sur OGSteam.fr";
+$lang['autoupdate_tableau_version'] = "Version installée";
+$lang['autoupdate_tableau_versionSVN'] = "Dernière version disponible";
+$lang['autoupdate_tableau_versionTrunk'] = "Dernière version sur Trunk";
+$lang['autoupdate_tableau_action'] = "Action";
+$lang['autoupdate_tableau_uptodate'] = "Actualizar";
+$lang['autoupdate_tableau_install'] = "Instalar";
+$lang['autoupdate_tableau_norefered'] = "Non référencé";
+$lang['autoupdate_tableau_link'] = "Liens";
+$lang['autoupdate_tableau_bug'] = "En cas de problème:";
+$lang['autoupdate_tableau_buglink'] = "Soumettre un problème";
+$lang['autoupdate_tableau_pageadmin'] = "Page d'administration des modules OGSpy";
+$lang['autoupdate_tableau_uptodateok'] = "La actualización se ha completado";
+$lang['autoupdate_tableau_installok'] = "Installation effectuée";
+$lang['autoupdate_tableau_back'] = "Retour au tableau des mises à jour";
+$lang['autoupdate_tableau_uptodateoff'] = "Mise à jour impossible, passez par la page d'administration";
+$lang['autoupdate_tableau_installoff'] = "Installation impossible, passez par la page d'administration";
+$lang['autoupdate_MaJ_uptodateok'] = "Mise à jour réussie !!";
+$lang['autoupdate_MaJ_downok'] = "Téléchargement réussi.";
+$lang['autoupdate_MaJ_unzipok'] = "Décompression réussie.";
+$lang['autoupdate_MaJ_errorversionogspy'] = "Version d'OGSpy supérieure requise";
+$lang['autoupdate_MaJ_rights'] = "Vous n'avez pas les droits nécessaire.";
+$lang['autoupdate_MaJ_wantupdate'] = "Voulez vous continuer la mise à jour du mod ";
+$lang['autoupdate_MaJtool_wantbackup'] = "Merci de réaliser une sauvegarde de vos données avant la mise à jour !!";
+$lang['autoupdate_MaJtool_wantupdate'] = "Voulez vous continuer la mise à jour de l'outil ";
+$lang['autoupdate_MaJ_linkupdate'] = "Si";
+$lang['autoupdate_admin_valid'] = "Valider les paramètres";
+$lang['autoupdate_admin_option'] = "Option";
+$lang['autoupdate_admin_value'] = "Valor";
+$lang['autoupdate_admin_value1'] = "(oui | non)";
+$lang['autoupdate_admin_MaJ'] = "Mise à jour et téléchargement de nouveaux mods autorisé pour les co-admins";
+$lang['autoupdate_admin_trunk'] = "Proposer la mise à jour depuis le Trunk ?";
+$lang['autoupdate_admin_trunk1'] = "(Versions de développement)";
+$lang['autoupdate_admin_frequency'] = "Choisir la fréquence de mise à jour(Heures)";
+$lang['autoupdate_createdby'] = "Créé par";
+$lang['autoupdate_and'] = "et";
+$lang['autoupdate_version'] = "version";
+$lang['autoupdate_error'] = "ERREUR";
+
---
+++ lang/pt_BR/lang_autoupdate.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Autoupdate Language File
+ * @package [Mod] Autoupdate
+ * @subpackage i18n
+ * @author Bartheleway <contactbarthe@g.q-le-site.webou.net>
+ * @copyright Copyright &copy; 2016, http://ogsteam.fr/
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version 2.1.9
+ */
+
+$lang['autoupdate_autoupdate_table'] = "Tabela de atualização";
+$lang['autoupdate_autoupdate_admin'] = "Administração";
+$lang['autoupdate_autoupdate_down'] = "Download dos mods";
+$lang['autoupdate_tableau_info'] = "Quando os mods são atualizados, os ficheiros presentes no zip substituem os antigos.";
+$lang['autoupdate_tableau_error3'] = "Atenção, Autoupdate não é autorizado a escrever no diretório do mod e nem poderá instalar.";
+$lang['autoupdate_tableau_error4'] = "Não há versão de produção disponível para este mod.";
+$lang['autoupdate_tableau_namemod'] = "Nome do módulo";
+$lang['autoupdate_tableau_authormod'] = "Autor do módulo";
+$lang['autoupdate_tableau_descmod'] = "Descrição";
+$lang['autoupdate_tableau_nametool'] = "Nome da ferramenta";
+$lang['autoupdate_tableau_authtool'] = "Autor da ferramenta";
+$lang['autoupdate_tableau_modinstall'] = "Módulos instalados - Pesquise a atualização no OGSteam.fr";
+$lang['autoupdate_tableau_toolinstall'] = "Ferramentas instaladas - Pesquise as atualizações no OGSteam.fr";
+$lang['autoupdate_tableau_modnoinstall'] = "Módulos não instalados - Pesquise os mods no OGSteam.fr";
+$lang['autoupdate_tableau_version'] = "Versão instalada";
+$lang['autoupdate_tableau_versionSVN'] = "Última versão disponível";
+$lang['autoupdate_tableau_versionTrunk'] = "Última versão no Trunk";
+$lang['autoupdate_tableau_action'] = "Ação";
+$lang['autoupdate_tableau_uptodate'] = "Atualizar";
+$lang['autoupdate_tableau_install'] = "Instalar";
+$lang['autoupdate_tableau_norefered'] = "Não referenciada";
+$lang['autoupdate_tableau_link'] = "Links";
+$lang['autoupdate_tableau_bug'] = "Em caso de problema:";
+$lang['autoupdate_tableau_buglink'] = "Submeter um problema";
+$lang['autoupdate_tableau_pageadmin'] = "Página de administração dos módulos OGSpy";
+$lang['autoupdate_tableau_uptodateok'] = "Atualização efetuada";
+$lang['autoupdate_tableau_installok'] = "Instalação efetuada";
+$lang['autoupdate_tableau_back'] = "Retornar à tabela de atualização";
+$lang['autoupdate_tableau_uptodateoff'] = "Atualização impossível, passe pela página de administração";
+$lang['autoupdate_tableau_installoff'] = "Instalação impossível, passe pela página de administração";
+$lang['autoupdate_MaJ_uptodateok'] = "Atualização bem sucedida !!";
+$lang['autoupdate_MaJ_downok'] = "Download bem sucedido.";
+$lang['autoupdate_MaJ_unzipok'] = "Descompactação bem sucedida.";
+$lang['autoupdate_MaJ_errorversionogspy'] = "Versão superior do OGSpy é necessária";
+$lang['autoupdate_MaJ_rights'] = "Você não possui os direitos necessários.";
+$lang['autoupdate_MaJ_wantupdate'] = "Você deseja continuar a atualização do mod";
+$lang['autoupdate_MaJtool_wantbackup'] = "Por favor realize um backup de seus dados antes da atualização !!";
+$lang['autoupdate_MaJtool_wantupdate'] = "Você deseja continuar a atualização da ferramenta";
+$lang['autoupdate_MaJ_linkupdate'] = "Sim";
+$lang['autoupdate_admin_valid'] = "Validar os parâmetros";
+$lang['autoupdate_admin_option'] = "Opção";
+$lang['autoupdate_admin_value'] = "Valor";
+$lang['autoupdate_admin_value1'] = "(sim | não)";
+$lang['autoupdate_admin_MaJ'] = "Atualização e download de novos mods autorizados pelos co-admins";
+$lang['autoupdate_admin_trunk'] = "Propor atualização a partir do Trunk ?";
+$lang['autoupdate_admin_trunk1'] = "(Versões de desenvolvimento)";
+$lang['autoupdate_admin_frequency'] = "Escolha a frequência de atualização (Horas)";
+$lang['autoupdate_createdby'] = "Criada pelo";
+$lang['autoupdate_and'] = "e";
+$lang['autoupdate_version'] = "versão";
+$lang['autoupdate_error'] = "ERRO";
+
