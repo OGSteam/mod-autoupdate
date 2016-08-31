46ef3f01a93c6295c1ab38385031039c52e852aa
Anthony <darknoon29@users.noreply.github.com>
2016-08-31 21:58:48 +0200

    Ajout Italien

---
+++ lang/it/lang_autoupdate.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Autoupdate Language File
+ * @package [Mod] Autoupdate
+ * @subpackage i18n
+ * @author Bartheleway <contactbarthe@g.q-le-site.webou.net>
+ * @copyright Copyright &copy; 2016, http://ogsteam.fr/
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version 2.1.9
+ */
+
+$lang['autoupdate_autoupdate_table'] = "Tableau de mise à jour";
+$lang['autoupdate_autoupdate_admin'] = "Amministrazione";
+$lang['autoupdate_autoupdate_down'] = "Téléchargement des mods";
+$lang['autoupdate_tableau_info'] = "Lorsque les mods sont mis à jour, les fichiers présents dans le zip écrasent les anciens.";
+$lang['autoupdate_tableau_error3'] = "Attention, Autoupdate n'est pas autorisé à écrire dans le répertoire mod et ne pourra rien installer";
+$lang['autoupdate_tableau_error4'] = "Pas de version de production disponible pour ce mod";
+$lang['autoupdate_tableau_namemod'] = "Nom du module";
+$lang['autoupdate_tableau_authormod'] = "Auteur du module";
+$lang['autoupdate_tableau_descmod'] = "Descrizione";
+$lang['autoupdate_tableau_nametool'] = "Nom de l'outil";
+$lang['autoupdate_tableau_authtool'] = "Auteur de l'outil";
+$lang['autoupdate_tableau_modinstall'] = "Modules installés - Recherche des mises à jour sur OGSteam.fr";
+$lang['autoupdate_tableau_toolinstall'] = "Outils installés - Recherche des mises à jour sur OGSteam.fr";
+$lang['autoupdate_tableau_modnoinstall'] = "Modules non installés - Recherche des mods sur OGSteam.fr";
+$lang['autoupdate_tableau_version'] = "Version installée";
+$lang['autoupdate_tableau_versionSVN'] = "Dernière version disponible";
+$lang['autoupdate_tableau_versionTrunk'] = "Dernière version sur Trunk";
+$lang['autoupdate_tableau_action'] = "Action";
+$lang['autoupdate_tableau_uptodate'] = "Mettre à jour";
+$lang['autoupdate_tableau_install'] = "Installer";
+$lang['autoupdate_tableau_norefered'] = "Non référencé";
+$lang['autoupdate_tableau_link'] = "Liens";
+$lang['autoupdate_tableau_bug'] = "En cas de problème:";
+$lang['autoupdate_tableau_buglink'] = "Soumettre un problème";
+$lang['autoupdate_tableau_pageadmin'] = "Page d'administration des modules OGSpy";
+$lang['autoupdate_tableau_uptodateok'] = "Mise à jour effectuée";
+$lang['autoupdate_tableau_installok'] = "Installation effectuée";
+$lang['autoupdate_tableau_back'] = "Retour au tableau des mises à jour";
+$lang['autoupdate_tableau_uptodateoff'] = "Mise à jour impossible, passez par la page d'administration";
+$lang['autoupdate_tableau_installoff'] = "Installation impossible, passez par la page d'administration";
+$lang['autoupdate_MaJ_uptodateok'] = "Mise à jour réussie !!";
+$lang['autoupdate_MaJ_downok'] = "Téléchargement réussi.";
+$lang['autoupdate_MaJ_unzipok'] = "Décompression réussie.";
+$lang['autoupdate_MaJ_errorversionogspy'] = "Version d'OGSpy supérieure requise";
+$lang['autoupdate_MaJ_rights'] = "Vous n'avez pas les droits nécessaire.";
+$lang['autoupdate_MaJ_wantupdate'] = "Voulez vous continuer la mise à jour du mod ";
+$lang['autoupdate_MaJtool_wantbackup'] = "Merci de réaliser une sauvegarde de vos données avant la mise à jour !!";
+$lang['autoupdate_MaJtool_wantupdate'] = "Voulez vous continuer la mise à jour de l'outil ";
+$lang['autoupdate_MaJ_linkupdate'] = "Si";
+$lang['autoupdate_admin_valid'] = "Valider les paramètres";
+$lang['autoupdate_admin_option'] = "Option";
+$lang['autoupdate_admin_value'] = "Valore";
+$lang['autoupdate_admin_value1'] = "(oui | non)";
+$lang['autoupdate_admin_MaJ'] = "Mise à jour et téléchargement de nouveaux mods autorisé pour les co-admins";
+$lang['autoupdate_admin_trunk'] = "Proposer la mise à jour depuis le Trunk ?";
+$lang['autoupdate_admin_trunk1'] = "(Versions de développement)";
+$lang['autoupdate_admin_frequency'] = "Choisir la fréquence de mise à jour(Heures)";
+$lang['autoupdate_createdby'] = "Créé par";
+$lang['autoupdate_and'] = "et";
+$lang['autoupdate_version'] = "version";
+$lang['autoupdate_error'] = "ERREUR";
+
